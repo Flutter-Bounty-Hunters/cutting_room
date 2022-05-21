58ef5f1719091072cc8b6042d7030a76dc9a82b4
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-05-20 23:00:14 -0700

    Added a logger, also WIP on asset path lookup

--- lib/src/assets.dart
+++ lib/src/assets.dart
@@ -1,4 +1,7 @@
+// ignore: deprecated_member_use
+import 'dart:cli';
 import 'dart:io';
+import 'dart:isolate';
 
 class Assets {
   static Assets? _instance;
@@ -9,21 +12,49 @@ class Assets {
 
   Assets._();
 
-  Directory? _assetsDirectory;
+  // Directory? _assetsDirectory;
+  final Map<String, String> _fileLookupCache = {};
 
   String getAssetPath(String assetFileName) {
-    if (_assetsDirectory == null) {
-      throw Exception('No assets directory was set before attempting to access: $assetFileName');
-    }
+    // TODO: delete the initial implementation below
+    // if (_assetsDirectory == null) {
+    //   throw Exception('No assets directory was set before attempting to access: $assetFileName');
+    // }
+    //
+    // return File('${_assetsDirectory!.path}/$assetFileName').path;
 
-    return File('${_assetsDirectory!.path}/$assetFileName').path;
+    if (_fileLookupCache.containsKey(assetFileName)) {
+      return _fileLookupCache[assetFileName]!;
+    } else {
+      final uri = Uri.parse('package:cutting_room/assets/$assetFileName');
+      final future = Isolate.resolvePackageUri(uri);
+
+      // waitFor is strongly discouraged in general, but it is accepted as the
+      // only reasonable way to load package assets outside of Flutter.
+      // ignore: deprecated_member_use
+      final package = waitFor(future, timeout: const Duration(seconds: 5));
+      if (package != null) {
+        final file = File.fromUri(package);
+        if (file.existsSync()) {
+          return file.path;
+        }
+      } else {
+        // Last ditch attempt: look in local folder
+        final file = File(assetFileName);
+        if (file.existsSync()) {
+          return file.path;
+        }
+      }
+    }
+    throw Exception('Could not find asset $assetFileName.');
   }
 
-  void setAssetsDirectory(Directory directory) {
-    if (!directory.existsSync()) {
-      throw Exception('Assets directory does not exist: ${directory.path}');
-    }
-
-    _assetsDirectory = directory;
-  }
+  // TODO: delete this when we get package asset path lookup working
+  // void setAssetsDirectory(Directory directory) {
+  //   if (!directory.existsSync()) {
+  //     throw Exception('Assets directory does not exist: ${directory.path}');
+  //   }
+  //
+  //   _assetsDirectory = directory;
+  // }
 }
